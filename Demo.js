f31eea570d88be86cd6883cbe085361634e6aca0
MostlyMagical <jb.mccann@outlook.com>
2022-08-30 00:02:32 +0100

    Second Commit

--- Demo.js
+++ Demo.js
@@ -1,5 +1,7 @@
-addUp = (num1, num2) => {
-    console.log(num1 + num2)
+Subtract = (num1, num2) => {
+    console.log(num1 - num2)
 }
 
-addUp(3, 6)
+Subtract(6, 3)
+
+//psst have a hint, its not here and its not in the fourth or final commit either.
