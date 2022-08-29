0adcd07ac8d9d1837f4b76400a23a53f9b999b17
MostlyMagical <jb.mccann@outlook.com>
2022-08-30 00:45:54 +0100

    Sixth Commit

--- Demo.js
+++ Demo.js
@@ -1,11 +1,7 @@
-Multiply = (num1, num2) => {
-    console.log(num1 * num2)
+Subtract = (num1, num2) => {
+    console.log(num1 - num2)
 }
 
-Multiply(1785, 8765)
+Subtract(1276, 32)
 
-//  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d
-//  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-//   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-//   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-//   pariatur
+//  Psssst here's a hint, the word is hidden in a commit after this one
