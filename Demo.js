22b4af5f8771f9565b74beb00b96287b751a2d8e
MostlyMagical <jb.mccann@outlook.com>
2022-08-30 00:47:45 +0100

    Seventh Commit

--- Demo.js
+++ Demo.js
@@ -1,7 +1,11 @@
-Subtract = (num1, num2) => {
-    console.log(num1 - num2)
+addUp = (num1, num2) => {
+    console.log(num1 + num2)
 }
 
-Subtract(1276, 32)
+addUp(15, 100000000)
 
-//  Psssst here's a hint, the word is hidden in a commit after this one
+//  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d
+//  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+//   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+//   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+//   pariatur
