02b03f5bef5fd75677c10dcbcfc1914d9a74f276
MostlyMagical <jb.mccann@outlook.com>
2022-08-30 00:55:05 +0100

    Ninth Commit

--- Demo.js
+++ Demo.js
@@ -1,11 +1,11 @@
-Multiply = (num1, num2) => {
-    console.log(num1 * num2)
+Divide = (num1, num2) => {
+    console.log(num1 / num2)
 }
 
-Multiply(3700000000, 12348900000000)
+Divide(3756, 8)
 
 //  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d
 //  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-//   veniam, quis nostrud exercitation SNEAKY ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+//   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
 //   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
 //   pariatur
