3fa91e0ef5ffe3442999ffa5d2ac8783a025af3d
MostlyMagical <jb.mccann@outlook.com>
2022-08-30 01:02:24 +0100

    Eleventh Commit

--- Demo.js
+++ Demo.js
@@ -1,7 +1,11 @@
-addUp = (num1, num2) => {
-    console.log(num1 + num2)
+Divide = (num1, num2) => {
+    console.log(num1 / num2)
 }
 
-addUp(17, 7)
+Divide(170, 7)
 
-//  Psssst here's another hint, the word is hidden in a commit with the Multiply function
+//  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d
+//  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+//   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+//   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+//   pariatur
