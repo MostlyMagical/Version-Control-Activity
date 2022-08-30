8615cd785c545f988d176591c377e888120ec4a8
MostlyMagical <jb.mccann@outlook.com>
2022-08-30 01:04:53 +0100

    Final Commit

--- Demo.js
+++ Demo.js
@@ -1,11 +1,7 @@
-Divide = (num1, num2) => {
-    console.log(num1 / num2)
+Subtract = (num1, num2) => {
+    console.log(num1 - num2)
 }
 
-Divide(170, 7)
+Subtract(10000000000, 2)
 
-//  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d
-//  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-//   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-//   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-//   pariatur
+//  Hey here's an unhelpful hint. its not here! 
